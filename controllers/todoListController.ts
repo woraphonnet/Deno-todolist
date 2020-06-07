104c402b10659a059df621c2c8d25ae6854cbb0a
Woraphon Netnim <woraphon.tdev@gmail.com>
2020-06-07 11:23:51 +0700

    update

--- controllers/todoListController.ts
+++ controllers/todoListController.ts
@@ -1,6 +1,5 @@
 import ToDoModel from "../model/todo.ts";
-
-type NewType = ToDoModel;
+import { DB } from "https://deno.land/x/sqlite/mod.ts";
 
 class todoList {
   public getAllData() {
@@ -18,13 +17,15 @@ class todoList {
     return data;
   }
 
-  public async updateData(id:any,obj: any) {
-    console.log('obj',obj);
+  public async updateData(id:any,obj: object) {
     try {
-      await ToDoModel.where("title","xxxx").update({ title: "guide hi", detail: "asdasdasd" });
+      const db = new DB("database.sqlite");
+      db.query("UPDATE todo_list SET title ='guide' WHERE id = 3");
+      db.close();
+      // await ToDoModel.where('id','3').update('{title:"guide hi",detail:"asdasdasd"}');
       return "asdasds";          
     } catch (error) {
-      console.log(error);
+      console.log("asdasd",error);
     }
   }
 
